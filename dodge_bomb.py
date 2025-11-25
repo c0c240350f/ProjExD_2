0e44fd8a0b806226a37255553541ecebdefc44b1
suda <c0c240350f@edu.teu.ac.jp>
2025-11-25 14:38:07 +0900

    練習３壁

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,16 +4,28 @@ import random
 import pygame as pg
 
 
-
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
     pg.K_UP: (0, -5),
-    pg.K_DOWN: (0, 5),
+    pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
-    pg.K_RIGHT: (5, 0)
+    pg.K_RIGHT: (+5, 0)
 }
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def check_bound(rct:pg.Rect) -> tuple[bool,bool]:
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル（横方向判定結果、縦方向判定結果）
+    画面内ならTrue、画面外ならFalse
+    """
+    yoko,tate=True,True
+    if rct.left<0 or WIDTH<rct.right: #横方向のはみだしチェック
+        yoko=False
+    if rct.top<0 or HEIGHT<rct.bottom: #横方向のはみだしチェック
+        tate=False
+    return yoko,tate
 
 
 def main():
@@ -25,6 +37,7 @@ def main():
     kk_rct.center = 300, 200
     bb_img=pg.Surface((20,20)) #空のsurface
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)#半径10の赤い円を描画
+    bb_img.set_colorkey((0,0,0)) #黒色を透明に
     bb_rct=bb_img.get_rect()#爆弾rect
     bb_rct.center = random.randint(0,WIDTH),random.randint(0,WIDTH) #爆弾の座標
     vx,vy=+5,+5 #爆弾の横速度、縦速度
@@ -55,7 +68,14 @@ def main():
                 sum_mv[0]+=mv[0] #横方向の移動
                 sum_mv[1]+=mv[1] #縦方向の移動
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) !=(True,True):#画面外なら
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        yoko,tate=check_bound(bb_rct)
+        if not yoko: #横方向にはみ出していたら
+            vx *=-1
+        if not tate: #縦方向にはみ出していたら
+            vy *=-1
         bb_rct.move_ip(vx,vy)
         screen.blit(bb_img,bb_rct)
         pg.display.update()

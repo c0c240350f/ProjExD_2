5517552e1486a81be9ec89e1dc0d3656129aaf6f
suda <c0c240350f@edu.teu.ac.jp>
2025-11-25 14:08:46 +0900

    練習①

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,9 +1,18 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
+
 WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, 5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (5, 0)
+}
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -14,6 +23,11 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img=pg.Surface((20,20)) #空のsurface
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)#半径10の赤い円を描画
+    bb_rct=bb_img.get_rect()#爆弾rect
+    bb_rct.center = random.randint(0,WIDTH),random.randint(0,WIDTH) #爆弾の座標
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -24,14 +38,20 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+        
+
+        for key,mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0]+=mv[0] #横方向の移動
+                sum_mv[1]+=mv[1] #縦方向の移動
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
